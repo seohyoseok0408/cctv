ae04ce0f9e1fb6d7dc196af29c708d146e8d18e3
seohyoseok0408 <seohyoseok0408@gmail.com>
2024-03-30 03:52:28 +0900

    3.30

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,24 @@
 import styles from './Home.module.css';
-import Cctv from './components/Cctv.js';
-import Smallcctv from './components/Smallcctv.js';
-import Kakao from './components/Kakao.js';
+import Cctv from '../components/Cctv.js';
+import Smallcctv from '../components/Smallcctv.js';
+import Kakao from '../components/Kakao.js';
+import Btn_menu from '../components/sidebar/Btn_menu.js';
+import Test_cctv from '../components/Test_cctv.js';
 function Home() {
 
   return (
     <div className={styles.mainContainer}>
       <header className={styles.menubar}>
         <h1 className={styles.logo_box}>
-          <a className={styles.link_logo} href='http://localhost:3000/'></a>
+          <a href='http://localhost:3000/'>
+            <img className={styles.home_icon} src="image\baseline_home_black_24dp.jpg" alt="logo" />
+          </a>
         </h1>
-        <nav>
-          <ul className={styles.list_navbar}>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-
-        </nav>
+        <ul className={styles.list_navbar}>
+          <Btn_menu img_src={"image/baseline_search_black_24dp.jpg"} alt_text="search" />
+          <Btn_menu img_src={"image/baseline_star_black_24dp.jpg"} alt_text="star" />
+          <Btn_menu img_src={"image/baseline_more_horiz_black_24dp.jpg"} alt_text="star" />
+        </ul>
       </header>
       <div className={styles.container}>
         <div className={styles.map_wrap}>
@@ -30,13 +30,7 @@ function Home() {
             <Cctv />
             <Cctv />
             <Cctv />
-            <Cctv />
-            <Cctv />
-            <Cctv />
-            <Cctv />
-            <Cctv />
-            <Cctv />
-            <Cctv />
+            <Test_cctv />
           </div>
           <button class="next-button">다음</button>
           <div className={styles.smallContainer}>
